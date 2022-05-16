e796ba94223a6f6891a191f3761e0c3ffc033076
Russell Andrew Edson <russell.andrew.edson@pm.me>
2022-05-16 11:40:56 +0930

    Ancillary script for scraping the output XML for variables, e.g.

---
+++ list_outputs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Quick script to scrape the generated XML from the Schedule 3
+# project metadata and generate a list of outputs. Use with
+#   ./list_outputs.sh XML/
+#
+# Code author: Russell A. Edson, Biometry Hub
+# Date last modified: 16/05/2022
+IFS=$'\n'
+
+OUTPUTS=''
+for FILE in $(find "$1" . -type f -name "*.xml")
+do
+  VARIABLES=$(grep -oP '(?<=Variables: )[^\n]*(?=</p>)' "$FILE")
+  OUTPUTS="$OUTPUTS,$VARIABLES"
+done
+
+echo "$OUTPUTS" | sed 's/,[ ]*/\n/g'
